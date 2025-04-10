a6f1164797bbbf37941d419799224a1f100cb9f7
Hieu Tran <ngochieutq2001@gmail.com>
2025-04-10 23:17:00 +0700

    init: setup laravel 12 + docker + user seeder

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Seller',
+            'email' => 'seller@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'seller',
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]);
+    }
+}
